8fe16463ba67cdfefbfa0feae61d72cac54582b0
Erick Rashon <erick.rashon@jicohen.com>
2026-07-10 16:22:42 -0600

    Initial commit: Web API skeleton with Scalar UI and Serilog request logging
    Base .NET 10 project before the SAP EWM <-> KiSoft One middleware rewrite.

---
+++ KnappMiddleware/Program.cs
@@ -0,0 +1,34 @@
+using Scalar.AspNetCore;
+using Serilog;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Host.UseSerilog((context, services, configuration) => configuration
+    .ReadFrom.Configuration(context.Configuration)
+    .ReadFrom.Services(services));
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+    app.MapScalarApiReference(options => options.WithClassicLayout());
+    app.MapGet("/", () => Results.Redirect("/scalar"));
+}
+
+app.UseSerilogRequestLogging();
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
